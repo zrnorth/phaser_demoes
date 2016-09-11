e4927b807e17eff2242089d4edc6b3bf839369bd
Zach North <znorth@ZNORT1ML1.local>
2016-09-10 18:02:05 -0700

    added spessman collectables

--- spessman/js/game.js
+++ spessman/js/game.js
@@ -26,9 +26,12 @@ Spessman.Game.prototype = {
         this.explosionSound = this.game.add.audio('explosion');
         this.collectSound = this.game.add.audio('collect');
 
-        // Create some asteroids
+        // Create some asteroids and collectables
         this.generateAsteroids();
+        this.generateCollectables();
+
     },
+
     update: function() {
         if (this.game.input.activePointer.justPressed()) {
             // move in the dir of the input
@@ -39,13 +42,14 @@ Spessman.Game.prototype = {
 
         // If player hits an asteroid, should track collision
         this.game.physics.arcade.collide(this.player, this.asteroids, this.hitAsteroid, null, this);
+
+        // if player picks up a collectable, should delete collectable and update score
+        this.game.physics.arcade.overlap(this.player, this.collectables, this.collect, null, this);
     },
+
     generateAsteroids: function() {
         this.asteroids = this.game.add.group();
-        
-        //physics
-        this.asteroids.enableBody = true;
-        this.asteroids.physicsBodyType = Phaser.Physics.ARCADE;
+        this.setObjectPhysics(this.asteroids);
 
         // Create an rng for each asteroid to determine its movement.
         var numAsteroids = this.game.rnd.integerInRange(150, 200);
@@ -63,10 +67,50 @@ Spessman.Game.prototype = {
             asteroid.body.collideWorldBounds = true;
         }
     },
+
+    generateCollectables: function() {
+        this.collectables = this.game.add.group();
+        this.setObjectPhysics(this.collectables);
+        
+        var numCollectables = this.game.rnd.integerInRange(100, 150);
+        var collectable;
+
+        for (var i = 0; i < numCollectables; i++) {
+            collectable = this.collectables.create(this.game.world.randomX, this.game.world.randomY, 'power');
+            collectable.animations.add('fly', [0, 1, 2, 3], 5, true);
+            collectable.animations.play('fly');
+        }
+    },
+
+    // helper to handle setting physics
+    setObjectPhysics: function(objects) {
+        objects.enableBody = true;
+        objects.physicsBodyType = Phaser.Physics.ARCADE;
+    },
+
     hitAsteroid: function(player, asteroid) {
         this.explosionSound.play();
+
+        // Player explodes
+        var emitter = this.game.add.emitter(this.player.x, this.player.y, 100);
+        emitter.makeParticles('playerParticle');
+        emitter.minParticleSpeed.setTo(-200, -200);
+        emitter.maxParticleSpeed.setTo(200, 200);
+        emitter.gravity = 0;
+        emitter.start(true, 1000, null, 100);
         this.player.kill();
-        
+
+        // In 0.8sec, call gameOver 
         this.game.time.events.add(800, this.gameOver, this);
+    },
+
+    collect: function(player, collectable) {
+        this.collectSound.play();
+        this.playerScore++;
+        collectable.kill();
+    },
+
+    gameOver: function() { // return to main menu
+        this.game.state.start('MainMenu', true, false);
     }
 };
